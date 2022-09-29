87f49e781b596f1d4daafe994087cd1553e0005c
Chris Anderson <jchris@gmail.com>
2022-09-29 07:13:10 -0700

    basic uploads list

--- examples/react/file-upload/src/App.js
+++ examples/react/file-upload/src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import { AuthProvider, useAuth } from '@w3ui/react-wallet'
 import { UploaderProvider } from '@w3ui/react-uploader'
+import { UploadsListProvider } from '@w3ui/react-uploads-list'
+
 import ContentPage from './ContentPage'
 import logo from './logo.png'
 
@@ -8,16 +10,18 @@ function App () {
   return (
     <AuthProvider>
       <UploaderProvider>
-        <IdentityLoader>
-          <div className='flex flex-column justify-center items-center sans-serif light-silver'>
-            <header>
-              <img src={logo} width='250' alt='logo' />
-            </header>
-            <div className='w-90 w-50-ns mw6'>
-              <ContentPage />
+        <UploadsListProvider>
+          <IdentityLoader>
+            <div className='flex flex-column justify-center items-center sans-serif light-silver'>
+              <header>
+                <img src={logo} width='250' alt='logo' />
+              </header>
+              <div className='w-90 w-50-ns mw6'>
+                <ContentPage />
+              </div>
             </div>
-          </div>
-        </IdentityLoader>
+          </IdentityLoader>
+        </UploadsListProvider>
       </UploaderProvider>
     </AuthProvider>
   )
--- examples/react/file-upload/src/ContentPage.js
+++ examples/react/file-upload/src/ContentPage.js
@@ -1,20 +1,39 @@
+// eslint-disable-next-line no-redeclare
 /* global Blob, File */
 
 import React, { useState, useRef } from 'react'
 import { useUploader } from '@w3ui/react-uploader'
+import { useUploadsList } from '@w3ui/react-uploads-list'
 import { withIdentity } from './components/Authenticator'
 import { Camera } from 'react-camera-pro'
 
 import './spinner.css'
 
 function dataURLtoFile (dataurl) {
-  const arr = dataurl.split(','); const mime = arr[0].match(/:(.*?);/)[1]
-  const bstr = atob(arr[1]); let n = bstr.length; const u8arr = new Uint8Array(n)
+  const arr = dataurl.split(',')
+  const mime = arr[0].match(/:(.*?);/)[1]
+  const bstr = atob(arr[1])
+  let n = bstr.length
+  const u8arr = new Uint8Array(n)
   while (n--) {
     u8arr[n] = bstr.charCodeAt(n)
   }
   const blob = new Blob([u8arr], { type: mime })
   return new File([blob], 'camera-image')
+}
+
+function ImageListItem ({ cid, data }) {
+  if (/bagb/.test(`${cid}`)) {
+    return <li key={cid}>CAR cid: {cid}</li>
+  }
+  const imgSrc = data || `https://w3s.link/ipfs/${cid}`
+  return (
+    <li key={cid}>
+      <a href={`https://w3s.link/ipfs/${cid}`}>
+        <img alt='camera output' src={imgSrc} />
+      </a>
+    </li>
+  )
 }
 
 export function ContentPage () {
@@ -23,11 +42,15 @@ export function ContentPage () {
   const [error, setError] = useState(null)
   const camera = useRef(null)
   const [images, setImages] = useState([])
+  // eslint-disable-next-line no-unused-vars
+  const { loading, error: listError, data: listData, reload: listReload } = useUploadsList()
+
+  console.log('listData', listData)
 
   if (!uploader) return null
 
   // refactor to be pure func with return values?
-  const takePhoto = async e => {
+  const takePhoto = async (e) => {
     e.preventDefault()
     const imgdata = camera.current.takePhoto()
 
@@ -40,7 +63,7 @@ export function ContentPage () {
       setImages([{ cid: cid, data: imgdata }, ...images])
 
       // Upload the DAG to the service.
-      setStatus('uploading')
+      setStatus('uploading') // technically each upload has it's own status
       await uploader.uploadCar(car)
     } catch (err) {
       console.error(err)
@@ -50,22 +73,21 @@ export function ContentPage () {
     }
   }
 
-  const printStatus = (status === 'done' && error) ? error : status
+  const printStatus = status === 'done' && error ? error : status
 
   return (
     <div>
-      <p><button onClick={takePhoto}>Take photo</button> {printStatus}</p>
+      <p>
+        <button onClick={takePhoto}>Take photo</button> {printStatus}
+      </p>
       <Camera ref={camera} />
 
-      {images.map(({ cid, data }) => {
-        return (
-          <li key={cid}>
-            <a href={`https://w3s.link/ipfs/${cid}`}>
-              <img alt='camera output' src={data} />
-            </a>
-          </li>
-        )
-      })}
+      {images.map(({ cid, data }) => (
+        <ImageListItem key={cid} cid={cid} data={data} />
+      ))}
+      {listData.map((cid) => (
+        <ImageListItem key={cid} cid={cid} />
+      ))}
     </div>
   )
 }
