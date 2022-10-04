4189ba3154933759a60a1f6991924edf67d42ee8
joshJarr <josh.jarvis@potatolondon.com>
2022-10-04 10:57:53 +0100

    fix: Run linter fix, removed undefined CID when uploading.

--- examples/vanilla/file-upload/src/main.js
+++ examples/vanilla/file-upload/src/main.js
@@ -3,27 +3,26 @@ import { RegisterForm } from './register'
 import { UploadFileForm } from './upload'
 
 export const EVENTS = {
-  registrationSuccess: "registration:success",
-};
+  registrationSuccess: 'registration:success'
+}
 
 const SELECTORS = {
   registerComponent: '#register-form-component',
-  uploadComponent: '#upload-form-component',
+  uploadComponent: '#upload-form-component'
 }
 
 document.addEventListener(EVENTS.registrationSuccess, (event) => {
-  console.log('I HEAR YOU', event);
-  const registerEl = document.querySelector(SELECTORS.registerComponent);
+  const registerEl = document.querySelector(SELECTORS.registerComponent)
   // Switch components
-  registerEl.remove();
-  const uploadEl = document.createElement("upload-form")
-  const container = document.querySelector("#app div");
-  container.appendChild(uploadEl);
+  registerEl.remove()
+  const uploadEl = document.createElement('upload-form')
+  const container = document.querySelector('#app div')
+  container.appendChild(uploadEl)
 })
 
 // Create a listener for the sign in complete event
 
 export {
   RegisterForm,
-  UploadFileForm,
+  UploadFileForm
 }
--- examples/vanilla/file-upload/src/upload.js
+++ examples/vanilla/file-upload/src/upload.js
@@ -1,6 +1,5 @@
-import { EVENTS } from './main'
 import { loadDefaultIdentity } from '@w3ui/wallet-core'
-import { uploadCarChunks, encodeFile, chunkBlocks, encodeDirectory } from '@w3ui/uploader-core'
+import { uploadCarChunks, encodeFile, chunkBlocks } from '@w3ui/uploader-core'
 
 const SELECTORS = {
   uploadForm: '#upload-form',
@@ -8,34 +7,30 @@ const SELECTORS = {
   encodingTemplate: '#file-encoding-template',
   uploadingTemplate: '#file-uploading-template',
   uploadCompleteTemplate: '#upload-complete-template',
-  uploadErrorTemplate: '#upload-error-template',
+  uploadErrorTemplate: '#upload-error-template'
 }
 
 export class UploadFileForm extends HTMLElement {
-  constructor() {
-    super();
+  constructor () {
+    super()
     // this.identity = null;
     // this.email = null;
-    this.form$ = document.querySelector(SELECTORS.uploadForm);
-    this.uploadFormTemplate$ = document.querySelector(SELECTORS.uploadFormTemplate);
-    this.encodingTemplate$ = document.querySelector(SELECTORS.encodingTemplate);
-    this.uploadingTemplate$ = document.querySelector(SELECTORS.uploadingTemplate);
-    this.uploadCompleteTemplate$ = document.querySelector(SELECTORS.uploadCompleteTemplate);
-    this.uploadErrorTemplate$ = document.querySelector(SELECTORS.uploadErrorTemplate);
-
-    // this.submitHandler = this.submitHandler.bind(this);
-    // this.cancelRegistrationHandler = this.cancelRegistrationHandler.bind(this);
-    // this.signOutHandler = this.signOutHandler.bind(this);
-    this.formatTemplateContent = this.formatTemplateContent.bind(this);
+    this.form$ = document.querySelector(SELECTORS.uploadForm)
+    this.uploadFormTemplate$ = document.querySelector(SELECTORS.uploadFormTemplate)
+    this.encodingTemplate$ = document.querySelector(SELECTORS.encodingTemplate)
+    this.uploadingTemplate$ = document.querySelector(SELECTORS.uploadingTemplate)
+    this.uploadCompleteTemplate$ = document.querySelector(SELECTORS.uploadCompleteTemplate)
+    this.uploadErrorTemplate$ = document.querySelector(SELECTORS.uploadErrorTemplate)
+    this.formatTemplateContent = this.formatTemplateContent.bind(this)
   }
 
-  async handleFileUpload(event) {
-    event.preventDefault();
-    const fileInputEl = this.form$.querySelector("input[type=file");
-    const file = fileInputEl.files[0];
-    this.file = file;
+  async handleFileUpload (event) {
+    event.preventDefault()
+    const fileInputEl = this.form$.querySelector('input[type=file')
+    const file = fileInputEl.files[0]
+    this.file = file
 
-    this.identity = await loadDefaultIdentity();
+    this.identity = await loadDefaultIdentity()
 
     if (this.identity) {
       console.log(`DID2: ${this.identity.signingPrincipal.did()}`)
@@ -44,67 +39,68 @@ export class UploadFileForm extends HTMLElement {
     }
 
     try {
-      this.toggleEncoding();
-      const { cid, blocks } = await encodeFile(file);
-      const chunks = await chunkBlocks(blocks);
-      this.toggleUploading();
+      this.toggleEncoding()
+      const { cid, blocks } = await encodeFile(file)
+      const chunks = await chunkBlocks(blocks)
+      this.toggleUploading()
       await uploadCarChunks(this.identity.signingPrincipal, chunks)
-      this.cid = await cid;
-    } catch(error) {
-      this.toggleUploadError();
+      this.cid = await cid
+    } catch (error) {
+      this.toggleUploadError()
     } finally {
-      this.toggleUploadComplete();
+      this.toggleUploadComplete()
     }
   }
 
-  toggleEncoding() {
-    const templateContent = this.encodingTemplate$.content;
-    this.replaceChildren(this.formatTemplateContent(templateContent));
+  toggleEncoding () {
+    const templateContent = this.encodingTemplate$.content
+    this.replaceChildren(this.formatTemplateContent(templateContent))
   }
 
-  toggleUploading() {
-    const templateContent = this.uploadingTemplate$.content;
-    this.replaceChildren(this.formatTemplateContent(templateContent));
+  toggleUploading () {
+    const templateContent = this.uploadingTemplate$.content
+    this.replaceChildren(this.formatTemplateContent(templateContent))
   }
 
-  toggleUploadComplete() {
-    const templateContent = this.uploadCompleteTemplate$.content;
-    this.replaceChildren(this.formatTemplateContent(templateContent));
+  toggleUploadComplete () {
+    const templateContent = this.uploadCompleteTemplate$.content
+    this.replaceChildren(this.formatTemplateContent(templateContent))
   }
 
-  toggleUploadError() {
-    const templateContent = this.uploadErrorTemplate$.content;
-    this.replaceChildren(this.formatTemplateContent(templateContent));
+  toggleUploadError () {
+    const templateContent = this.uploadErrorTemplate$.content
+    this.replaceChildren(this.formatTemplateContent(templateContent))
   }
 
-  formatTemplateContent(templateContent) {
-    let slot;
+  formatTemplateContent (templateContent) {
+    let slot
     slot = templateContent.querySelector('[data-root-cid-slot]')
     if (slot) {
-      slot.innerText = this.cid;
+      slot.innerText = this.cid || ''
     }
-    slot = templateContent.querySelector('[data-file-name-slot]')
-    if (slot) {
-      slot.innerText = this.file.name;
+    const fileNameSlot = templateContent.querySelector('[data-file-name-slot]')
+    if (fileNameSlot) {
+      console.log(this.file)
+      fileNameSlot.innerText = this.file.name
     }
     slot = templateContent.querySelector('[data-error-messages-slot]')
-    if (slot) { 
-      slot.innerText = this.errors;
+    if (slot) {
+      slot.innerText = this.errors
     }
     return templateContent
   }
 
-  async connectedCallback() {
-    const templateContent = this.uploadFormTemplate$.content;
-    this.replaceChildren(templateContent);
-    this.handleFileUpload = this.handleFileUpload.bind(this);
-    this.form$ = document.querySelector(SELECTORS.uploadForm);
-    this.form$.addEventListener("submit", this.handleFileUpload);
+  async connectedCallback () {
+    const templateContent = this.uploadFormTemplate$.content
+    this.replaceChildren(templateContent)
+    this.handleFileUpload = this.handleFileUpload.bind(this)
+    this.form$ = document.querySelector(SELECTORS.uploadForm)
+    this.form$.addEventListener('submit', this.handleFileUpload)
   }
 
-  disconnectedCallback() {
-    this.form$.removeEventListener("submit", this.handleFileUpload);
+  disconnectedCallback () {
+    this.form$.removeEventListener('submit', this.handleFileUpload)
   }
 }
 
-customElements.define("upload-form", UploadFileForm);
+customElements.define('upload-form', UploadFileForm)
