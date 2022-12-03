a6f31febeb0df3e1ee1e3768e664d346e51ba8e2
Travis Vachon <travis.vachon@gmail.com>
2022-12-02 16:34:06 -0800

    feat!: rework UploaderComponentContext
    * make it work more like the UploaderContext in the providers/Uploader.tsx
    * rename it from UploaderContext to avoid a name collision
    * be more careful with memoizing the provider value to avoid unecessary re-renders from inline object literal creation (TODO: need to do this in the provider too)

--- packages/react-uploader/src/Uploader.tsx
+++ packages/react-uploader/src/Uploader.tsx
@@ -1,31 +1,43 @@
-import React, { useContext, createContext, useState } from 'react'
+import React, { useContext, useMemo, createContext, useState } from 'react'
 import { Link, Version } from 'multiformats'
-import { CARMetadata } from '@w3ui/uploader-core'
+import { CARMetadata, UploaderContextState, UploaderContextActions } from '@w3ui/uploader-core'
 import { useUploader } from './providers/Uploader'
 
-export type UploaderContextValue = {
-  storedDAGShards?: CARMetadata[],
-  file?: File,
-  setFile: React.Dispatch<React.SetStateAction<File | undefined>>,
-  dataCid?: Link<unknown, number, number, Version>,
+export type UploaderComponentContextState = UploaderContextState & {
   status?: string,
   error?: any,
+  file?: File,
   handleUploadSubmit?: (e: Event) => void
-
+  dataCid?: Link<unknown, number, number, Version>,
+  storedDAGShards?: CARMetadata[],
 }
 
-export const UploaderContext = createContext<UploaderContextValue>({
-  setFile: () => { }
-})
+export type UploaderComponentContextActions = UploaderContextActions & {
+  setFile: React.Dispatch<React.SetStateAction<File | undefined>>
+}
 
-export type UploaderProps = {
-  children: React.ReactNode,
+export type UploaderComponentContextValue = [
+  state: UploaderComponentContextState,
+  actions: UploaderComponentContextActions
+]
+
+const UploaderComponentContext = createContext<UploaderComponentContextValue>([
+  { storedDAGShards: [] },
+  {
+    setFile: () => { throw new Error('missing set file function') },
+    uploadFile: async () => { throw new Error('missing uploader context provider') },
+    uploadDirectory: async () => { throw new Error('missing uploader context provider') }
+  }
+])
+
+export type HeadlessUploaderProps = {
+  children?: JSX.Element,
 }
 
 export const Uploader = ({
   children,
-}: UploaderProps) => {
-  const [{ storedDAGShards }, uploader] = useUploader()
+}: HeadlessUploaderProps) => {
+  const [uploaderState, uploaderActions] = useUploader()
   const [file, setFile] = useState<File>()
   const [dataCid, setDataCid] = useState<Link<unknown, number, number, Version>>()
   const [status, setStatus] = useState('')
@@ -36,37 +48,49 @@ export const Uploader = ({
     if (file) {
       try {
         setStatus('uploading')
-        const cid = await uploader.uploadFile(file)
+        const cid = await uploaderActions.uploadFile(file)
         setDataCid(cid)
       } catch (err: any) {
-        console.error(err)
         setError(err)
       } finally {
         setStatus('done')
       }
     }
   }
+
+  const uploaderComponentContextValue = useMemo<UploaderComponentContextValue>(() => [
+    { ...uploaderState, file, dataCid, status, error, handleUploadSubmit },
+    { ...uploaderActions, setFile }
+  ], [uploaderState, file, dataCid, status, error, handleUploadSubmit, uploaderActions, setFile])
+
   return (
-    <UploaderContext.Provider value={{ storedDAGShards, file, setFile, dataCid, status, error, handleUploadSubmit }}>
+    <UploaderComponentContext.Provider value={uploaderComponentContextValue}>
       {children}
-    </UploaderContext.Provider>
+    </UploaderComponentContext.Provider>
   )
 }
 
 Uploader.Input = (props: any) => {
-  const { setFile } = useContext(UploaderContext)
+  const [, { setFile }] = useContext(UploaderComponentContext)
   return (
     <input type='file' onChange={e => setFile(e?.target?.files?.[0])} {...props} />
   )
 }
 
 Uploader.Form = ({ children, ...props }: { children: React.ReactNode } & any) => {
-  const { handleUploadSubmit } = useContext(UploaderContext)
+  const [{ handleUploadSubmit }] = useContext(UploaderComponentContext)
   return (
     <form onSubmit={handleUploadSubmit} {...props}>
       {children}
     </form>
   )
+}
+
+/**
+ * Use the scoped uploader context state from a parent `Uploader`.
+ */
+export function useUploaderComponent(): UploaderComponentContextValue {
+  return useContext(UploaderComponentContext)
 }
 
 export default Uploader
