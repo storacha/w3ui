642b68cec9e25e2016e011c83c3ac440f7345642
Travis Vachon <travis.vachon@gmail.com>
2022-12-15 15:47:07 -0800

    fix: update react uploads list example

--- examples/react/uploads-list/src/App.js
+++ examples/react/uploads-list/src/App.js
@@ -1,30 +1,30 @@
 import React, { useEffect } from 'react'
-import { AuthProvider, useAuth } from '@w3ui/react-keyring'
+import { KeyringProvider, useKeyring } from '@w3ui/react-keyring'
 import { UploadsListProvider } from '@w3ui/react-uploads-list'
 import ContentPage from './ContentPage'
 import logo from './logo.png'
 
 function App () {
   return (
-    <AuthProvider>
-      <UploadsListProvider>
-        <IdentityLoader>
+    <KeyringProvider>
+      <IdentityLoader>
+        <UploadsListProvider>
           <div className='vh-100 flex flex-column justify-center items-center sans-serif light-silver'>
             <header>
               <img src={logo} width='250' alt='logo' />
             </header>
             <ContentPage />
           </div>
-        </IdentityLoader>
-      </UploadsListProvider>
-    </AuthProvider>
+        </UploadsListProvider>
+      </IdentityLoader>
+    </KeyringProvider>
   )
 }
 
 function IdentityLoader ({ children }) {
-  const { loadDefaultIdentity } = useAuth()
+  const [, { loadAgent }] = useKeyring()
   // eslint-disable-next-line
-  useEffect(() => { loadDefaultIdentity() }, []) // try load default identity - once.
+  useEffect(() => { loadAgent() }, []) // try load default identity - once.
   return children
 }
 
