b606c39531534275e9480ec5d37b59fa6fbe7868
Adam Munns <adam@whetinteractive.com>
2022-12-20 09:49:16 -0800

    formatting fix

--- examples/react/sign-up-in/src/ContentPage.js
+++ examples/react/sign-up-in/src/ContentPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useKeyring } from '@w3ui/react-keyring'
 
-export default function ContentPage() {
+export default function ContentPage () {
   const [{ space }, { loadAgent, unloadAgent, createSpace, registerSpace, cancelRegisterSpace }] = useKeyring()
   const [email, setEmail] = useState('')
   const [submitted, setSubmitted] = useState(false)
@@ -23,7 +23,7 @@ export default function ContentPage() {
 
   if (submitted) {
     return (
-      <div className="w3ui-signUpIn__verify">
+      <div className='w3ui-signUpIn__verify'>
         <h1 className='w3ui-h1'>Verify your email address!</h1>
         <p className='w3ui-p'>Click the link in the email we sent to {email} to sign in.</p>
         <form onSubmit={e => { e.preventDefault(); cancelRegisterSpace() }}>
@@ -47,8 +47,8 @@ export default function ContentPage() {
   }
 
   return (
-    <div className="w3ui-signUpIn__login">
-      <form onSubmit={handleRegisterSubmit} className="w3ui-signUpIn__form">
+    <div className='w3ui-signUpIn__login'>
+      <form onSubmit={handleRegisterSubmit} className='w3ui-signUpIn__form'>
         <div className='w3ui-signUpIn__field'>
           <label htmlFor='email' className='w3ui-signUpIn__label w3ui-label'>Email address:</label>
           <input id='email' className='w3ui-signUpIn__input w3ui-input' type='email' value={email} onChange={e => setEmail(e.target.value)} required placeholder='youremail@address.com' />
