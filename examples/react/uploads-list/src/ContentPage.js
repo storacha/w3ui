1a4eecedad3c7adb431b5c4aebd7f40feca752d7
Adam Munns <adam@whetinteractive.com>
2022-12-21 09:40:07 -0800

    uploads list base style for ui

--- examples/react/uploads-list/src/ContentPage.js
+++ examples/react/uploads-list/src/ContentPage.js
@@ -7,43 +7,44 @@ export function ContentPage () {
   const [{ loading, error, data }, { next, reload }] = useUploadsList()
   useEffect(() => {
     next()
-  // we really only want to run this once so leave the deps list empty
+    // we really only want to run this once so leave the deps list empty
   }, [])
   if (error) {
     return <Errored error={error} />
   }
 
   return (
-    <div className='w-90 mw9'>
+    <div className={`w3ui-uploadsList-container ${loading && 'w3ui-refreshing'}`}>
       {data && data.length
         ? (
-          <div className='overflow-auto'>
-            <table className='w-100 mb3 collapse'>
-              <thead className='near-white tl'>
+          <div className='w3ui-table-container'>
+            <table className='w3ui-table'>
+              <thead className='w3ui-table_thead'>
                 <tr>
-                  <th className='pa3'>Upload CID</th>
+                  <th className='w3ui-table_th'>Upload CID</th>
                 </tr>
               </thead>
               <tbody>
                 {data.map(({ root }) => (
-                  <tr key={root.toString()} className='stripe-light'>
-                    <td className='pa3'>{root.toString()}</td>
+                  <tr key={root.toString()} className='w3ui-table_tr'>
+                    <td className='w3ui-table_td'>{root.toString()}</td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
           )
-        : <p className='tc'>No uploads</p>}
-      <button type='button' onClick={reload} className='ph3 pv2 mr3'>Refresh</button>
-      {loading ? <span className='spinner dib' /> : null}
+        : <p className='w3ui-table-empty-message'>No uploads</p>}
+      <button type='button' onClick={reload} className='w3ui-button w3ui-refresh'>
+        {loading ? 'Refreshing...' : 'Refresh'}
+      </button>
     </div>
   )
 }
 
 const Errored = ({ error }) => (
-  <div>
-    <h1 className='near-white'>⚠️ Error: failed to list uploads: {error.message}</h1>
+  <div className='w3ui-uploadsList-container w3ui-error'>
+    <h1 className='w3ui-error-header'>⚠️ Error: {error.message}</h1>
     <p>Check the browser console for details.</p>
   </div>
 )
