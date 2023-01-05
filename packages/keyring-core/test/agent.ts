cdcf653eea4637f579b39e6298b7d0a2a8ed09ca
Yusef Napora <yusef@napora.org>
2023-01-05 15:46:31 -0500

    wip: flesh out mock access service, test registerSpace

--- packages/keyring-core/test/agent.ts
+++ packages/keyring-core/test/agent.ts
@@ -4,6 +4,7 @@ import { JSDOM } from 'jsdom'
 import 'fake-indexeddb/auto'
 
 import { createAgent } from '../src/index.js'
+import { mockAcccessConnection } from './utils/mock-service.js'
 
 test.before((t) => {
   const dom = new JSDOM('<!DOCTYPE html>')
@@ -22,4 +23,20 @@ test('createSpace', async (t) => {
   const space = await agent.createSpace('test')
   t.truthy(space)
   t.true(space.did.startsWith('did:key:'))
+})
+
+test('registerSpace fails if no current space is set', async (t) => {
+  const agent = await createAgent()
+  await t.throwsAsync(agent.registerSpace('foo@bar.net'))
+})
+
+// TODO: finish access service mock and add mock websocket server for validation result
+test.skip('registerSpace invokes voucher/claim on access service', async (t) => {
+  const agent = await createAgent({
+    connection: await mockAcccessConnection()
+  })
+  const space = await agent.createSpace('test')
+  await agent.setCurrentSpace(space.did)
+
+  await agent.registerSpace('foo@bar.net')
 })
--- packages/keyring-core/test/utils/mock-service.ts
+++ packages/keyring-core/test/utils/mock-service.ts
@@ -1,5 +1,9 @@
-import { provide, Failure } from '@ucanto/server'
+import { create, provide, Failure } from '@ucanto/server'
+import * as Client from '@ucanto/client'
 import { Service } from '@web3-storage/access/types'
+
+import * as EdSigner from '@ucanto/principal/ed25519'
+import * as Transport from '@ucanto/transport'
 
 import * as Space from '@web3-storage/capabilities/space'
 import * as Voucher from '@web3-storage/capabilities/voucher'
@@ -7,25 +11,30 @@ import * as Voucher from '@web3-storage/capabilities/voucher'
 export class MockAccessService implements Service {
   voucher = {
     claim: provide(Voucher.claim, async () => {
+      console.log('voucher/claim')
       return undefined
     }),
 
     redeem: provide(Voucher.redeem, async () => {
-
+      console.log('voucher/redeem')
     })
   }
 
   space = {
     info: provide(Space.info, async () => {
+      console.log('space/info')
       return new Failure('not implemented')
     }),
     'recover-validation': provide(Space.recoverValidation, async () => {
+      console.log('space/recover-validation')
       return undefined
     }),
     recover: provide(Space.recover, async () => {
+      console.log('space/recover')
       return []
     })
   }
+
 
   constructor({} = {}) {
     // for (const [method, impl] of Object.keys(voucher ?? {})) {
@@ -36,4 +45,23 @@ export class MockAccessService implements Service {
     //   this.space[method] = impl
     // }  
   }
+}
+
+export async function mockAcccessConnection() {
+  const service = new MockAccessService()
+
+  const serverPrincipal = await EdSigner.generate()
+  const server = create({
+    id: serverPrincipal,
+    service,
+    encoder: Transport.CBOR,
+    decoder: Transport.CAR,
+  })
+  const client = Client.connect({
+    id: serverPrincipal,
+    channel: server,
+    decoder: Transport.CBOR,
+    encoder: Transport.CAR,
+  })
+  return client
 }
