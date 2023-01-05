747bceed06689742031762d9a496d51d9eb2c413
Yusef Napora <yusef@napora.org>
2023-01-05 11:58:36 -0500

    chore: add basic ava + typescript config

---
+++ ava.config.mjs
@@ -0,0 +1,9 @@
+export default {
+  files: [
+    "packages/**/test/**"
+  ],
+  extensions: ['js', 'ts'],
+  require: [
+    "ts-node/register"
+  ]
+}
---
+++ packages/keyring-core/test/hello.ts
@@ -0,0 +1,3 @@
+import test from 'ava'
+
+test('foo', t => t.pass())
