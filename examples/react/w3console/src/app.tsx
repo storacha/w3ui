860c0f960b2cb59ac7cb574d604684a18d97ce59
Travis Vachon <travis.vachon@gmail.com>
2023-01-11 15:54:30 -0800

    fix: tweak config and run linter
    bring this in line with our linter standards, and start it on port 3000 by default like our other apps

--- examples/react/w3console/src/app.tsx
+++ examples/react/w3console/src/app.tsx
@@ -2,21 +2,21 @@ import { useState } from 'preact/hooks'
 import preactLogo from './assets/preact.svg'
 import './app.css'
 
-export function App() {
+export function App (): JSX.Element {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
+          <img src='/vite.svg' class='logo' alt='Vite logo' />
         </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
+        <a href='https://preactjs.com' target='_blank' rel='noreferrer'>
+          <img src={preactLogo} class='logo preact' alt='Preact logo' />
         </a>
       </div>
       <h1>Vite + Preact</h1>
-      <div class="card">
+      <div class='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -24,7 +24,7 @@ export function App() {
           Edit <code>src/app.tsx</code> and save to test HMR
         </p>
       </div>
-      <p class="read-the-docs">
+      <p class='read-the-docs'>
         Click on the Vite and Preact logos to learn more
       </p>
     </>
