518a72d1704169c15df078a4b4fb71453161393e
Travis Vachon <travis.vachon@gmail.com>
2023-01-12 10:04:55 -0800

    feat: add tailwind for styling

--- examples/react/w3console/src/app.tsx
+++ examples/react/w3console/src/app.tsx
@@ -1,14 +1,15 @@
-import './app.css'
-
 import { SimpleAuthenticator, SimpleUploader, SimpleUploadsList, W3APIProvider } from '@w3ui/react-ui'
+import '@w3ui/react-ui/src/styles/uploader.css'
 
 export function App (): JSX.Element {
   return (
-    <W3APIProvider>
-      <SimpleAuthenticator>
-        <SimpleUploader />
-        <SimpleUploadsList />
-      </SimpleAuthenticator>
-    </W3APIProvider>
+    <main className='bg-slate-700'>
+      <W3APIProvider>
+        <SimpleAuthenticator>
+          <SimpleUploader />
+          <SimpleUploadsList />
+        </SimpleAuthenticator>
+      </W3APIProvider>
+    </main>
   )
 }
