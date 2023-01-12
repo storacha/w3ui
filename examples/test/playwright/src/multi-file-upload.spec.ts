dd94b6670c43806a840e1018bb1fe0ba96fe12e2
Oli Evans <oli@protocol.ai>
2023-01-12 11:59:10 +0000

    chore: lint and doc
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- examples/test/playwright/src/multi-file-upload.spec.ts
+++ examples/test/playwright/src/multi-file-upload.spec.ts
@@ -1,9 +1,9 @@
 import { test, expect } from '@playwright/test'
 
-for (const ui of ['react', 'solid', 'vue'/* 'vanilla' not work atm */]) {
-  test(`${ui}: sign in`, async ({ page }) => {
-    await page.goto(`/${ui}/sign-up-in/`)
-    await expect(page).toHaveTitle('W3UI Sign Up / Sign In Example App')
+for (const ui of ['react', 'solid'/* `vue` not exit yet, 'vanilla' not work atm */]) {
+  test(`${ui}: multi file upload`, async ({ page }) => {
+    await page.goto(`/${ui}/multi-file-upload/`)
+    await expect(page).toHaveTitle('W3UI Multi File Upload Example App')
 
     const input = page.getByRole('textbox', { name: 'Email address:' })
     await input.fill('test@example.org')
--- examples/test/playwright/src/uploads-list.spec.ts
+++ examples/test/playwright/src/uploads-list.spec.ts
@@ -1,9 +1,9 @@
 import { test, expect } from '@playwright/test'
 
 for (const ui of ['react', 'solid', 'vue'/* 'vanilla' not work atm */]) {
-  test(`${ui}: sign in`, async ({ page }) => {
-    await page.goto(`/${ui}/sign-up-in/`)
-    await expect(page).toHaveTitle('W3UI Sign Up / Sign In Example App')
+  test(`${ui}: uploads list`, async ({ page }) => {
+    await page.goto(`/${ui}/uploads-list/`)
+    await expect(page).toHaveTitle('W3UI Uploads List Example App')
 
     const input = page.getByRole('textbox', { name: 'Email address:' })
     await input.fill('test@example.org')
