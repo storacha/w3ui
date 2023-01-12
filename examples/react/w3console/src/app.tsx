af7928f7b560aea1f2041a2472818c8e2135a000
Travis Vachon <travis.vachon@gmail.com>
2023-01-11 16:33:56 -0800

    feat: add basic UI

--- examples/react/w3console/src/app.tsx
+++ examples/react/w3console/src/app.tsx
@@ -1,32 +1,14 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
 import './app.css'
 
-export function App (): JSX.Element {
-  const [count, setCount] = useState(0)
+import { SimpleAuthenticator, SimpleUploader, SimpleUploadsList, W3APIProvider } from '@w3ui/react-ui'
 
+export function App (): JSX.Element {
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src='/vite.svg' class='logo' alt='Vite logo' />
-        </a>
-        <a href='https://preactjs.com' target='_blank' rel='noreferrer'>
-          <img src={preactLogo} class='logo preact' alt='Preact logo' />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class='read-the-docs'>
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
+    <W3APIProvider>
+      <SimpleAuthenticator>
+        <SimpleUploader />
+        <SimpleUploadsList />
+      </SimpleAuthenticator>
+    </W3APIProvider>
   )
 }
