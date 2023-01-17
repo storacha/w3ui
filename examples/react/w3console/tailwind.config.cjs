1f15f35fa4db04065dd3f64cf13e56a685129f08
Oli Evans <oli@protocol.ai>
2023-01-17 16:56:15 +0000

    chore: add logo and custom dark-gray from w3ui-website
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- examples/react/w3console/tailwind.config.cjs
+++ examples/react/w3console/tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'gray-dark': '#1d2027'
+      },
+    }
   },
   plugins: []
 }
