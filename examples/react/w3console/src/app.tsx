814f78915fba42d94479d1c91cea4eedd82439a1
Oli Evans <oli@protocol.ai>
2023-01-19 16:35:44 +0000

    feat: adds space-finder autocomplete combobox
    - add a space-finder combobox based on https://headlessui.com/react/combobox
    - updates w3console layout to look fancy
    
    License: MIT
    Signed-off-by: Oli Evans <oli@protocol.ai>

--- examples/react/w3console/src/app.tsx
+++ examples/react/w3console/src/app.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, useEffect, useState } from 'react'
 import type { Space } from '@w3ui/keyring-core'
 
-import { Authenticator, Uploader, UploadsList, W3APIProvider } from '@w3ui/react'
+import { Authenticator, Uploader, UploadsList, W3APIProvider, SpaceFinder } from '@w3ui/react'
 import { useKeyring } from '@w3ui/react-keyring'
 import { useUploadsList } from '@w3ui/react-uploads-list'
 import md5 from 'blueimp-md5'
@@ -64,24 +64,31 @@ function SpaceSection (): JSX.Element {
   useEffect(() => { void reload() }, [space])
   const registered = Boolean(space?.registered())
   return (
-    <div className='container mx-auto'>
-      {registered
-        ? (
-          <>
-            <div className='flex flex-row space-x-4 mb-4 justify-between'>
-              <div className='shrink-0'>
-                {(space !== undefined) && (
-                  <img src={`https://www.gravatar.com/avatar/${md5(space.did())}?d=identicon`} className='w-20' />
-                )}
-              </div>
-              <Uploader onUploadComplete={() => { void reload() }} />
+    <div>
+      <header className='py-3'>
+        {(space !== undefined) && (
+          <div className='flex flex-row items-start gap-2'>
+            <img title={space.did()} src={`https://www.gravatar.com/avatar/${md5(space.did())}?d=identicon`} className='w-10 hover:saturate-200 saturate-0 invert border-solid border-gray-500 border' />
+            <div>
+              <h1 className='text-xl font-semibold leading-5'>{space.name() || 'Untitled'}</h1>
+              <label className='font-mono text-xs text-gray-500'>{space.did()}</label>
             </div>
-            <UploadsList />
+          </div>
+        )}
+        
+      </header>
+      <div className='container mx-auto'>
+        {registered ? (
+          <>
+            <Uploader onUploadComplete={() => { void reload() }} />
+            <div className='mt-8'>
+              <UploadsList />
+            </div>
           </>
-          )
-        : (
+        ) : (
           <SpaceRegistrar />
-          )}
+        )}
+      </div>
     </div>
   )
 }
@@ -112,11 +119,13 @@ function SpaceCreator (props: any): JSX.Element {
         ? (
           <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => { void onSubmit(e) }}>
             <input
+              className="text-black"
               type='email' placeholder='Email' autofocus
               value={email}
               onChange={(e: ChangeEvent<HTMLInputElement>) => { setEmail(e.target.value) }}
             />
             <input
+              className="text-black"
               placeholder='Name'
               value={name}
               onChange={(e: ChangeEvent<HTMLInputElement>) => { setName(e.target.value) }}
@@ -143,19 +152,19 @@ function SpaceSelector (props: any): JSX.Element {
     await setCurrentSpace(space.did())
   }
   return (
-    <div {...props}>
-      <h3 className='text-lg uppercase font-bold my-4'>Spaces</h3>
-      <ul className='space-y-2'>
-        {spaces.map((space, i) => (
-          <li key={space.did()} className={`hover:font-bold ${space.sameAs(currentSpace) ? 'font-bold' : ''}`}>
-            <button onClick={() => { void selectSpace(space) }}>
-              {space.name() ?? `Space ${i + 1}`}
-            </button>
-          </li>
-        ))}
-      </ul>
-      <SpaceCreator className='mt-12' />
+    <div>
+      <h3 className='text-xs tracking-wider uppercase font-bold my-2 text-gray-400 font-mono'>Spaces</h3>
+      <SpaceFinder spaces={spaces} selected={currentSpace} setSelected={selectSpace} />
     </div>
+  )
+}
+
+export function Logo (): JSX.Element {
+  return (
+    <h1 className='font-bold flex flex-row justify-center items-center gap-2'>
+      <svg className='site-logo-image text-white' width='30' viewBox='0 0 27.2 27.18' xmlns='http://www.w3.org/2000/svg'><path d='M13.6 27.18A13.59 13.59 0 1127.2 13.6a13.61 13.61 0 01-13.6 13.58zM13.6 2a11.59 11.59 0 1011.6 11.6A11.62 11.62 0 0013.6 2z' fill='currentColor' /><path d='M12.82 9.9v2.53h1.6V9.9l2.09 1.21.77-1.21-2.16-1.32 2.16-1.32-.77-1.21-2.09 1.21V4.73h-1.6v2.53l-2-1.21L10 7.26l2.2 1.32L10 9.9l.78 1.21zM18 17.79v2.52h1.56v-2.52L21.63 19l.78-1.2-2.16-1.33 2.16-1.28-.78-1.19-2.08 1.2v-2.58H18v2.56L15.9 14l-.77 1.2 2.16 1.32-2.16 1.33.77 1.15zM8.13 17.79v2.52h1.56v-2.52L11.82 19l.77-1.2-2.16-1.33 2.12-1.28-.73-1.24-2.13 1.23v-2.56H8.13v2.56L6.05 14l-.78 1.2 2.16 1.3-2.16 1.33.78 1.17z' fill='currentColor' /></svg>
+      console
+    </h1>
   )
 }
 
@@ -164,18 +173,18 @@ export function App (): JSX.Element {
     <W3APIProvider>
       <Authenticator>
         <div className='flex min-h-full w-full'>
-          <nav className='flex-none w-72 bg-white p-4 border-r border-gray-200'>
-            <div className='flex flex-col justify-start min-h-full'>
-              <div>
-                <h1 className='font-bold pb-4 flex flex-row justify-start items-center gap-2'>
-                  <svg className='site-logo-image text-black' width='30' viewBox='0 0 27.2 27.18' xmlns='http://www.w3.org/2000/svg'><path d='M13.6 27.18A13.59 13.59 0 1127.2 13.6a13.61 13.61 0 01-13.6 13.58zM13.6 2a11.59 11.59 0 1011.6 11.6A11.62 11.62 0 0013.6 2z' fill='currentColor' /><path d='M12.82 9.9v2.53h1.6V9.9l2.09 1.21.77-1.21-2.16-1.32 2.16-1.32-.77-1.21-2.09 1.21V4.73h-1.6v2.53l-2-1.21L10 7.26l2.2 1.32L10 9.9l.78 1.21zM18 17.79v2.52h1.56v-2.52L21.63 19l.78-1.2-2.16-1.33 2.16-1.28-.78-1.19-2.08 1.2v-2.58H18v2.56L15.9 14l-.77 1.2 2.16 1.32-2.16 1.33.77 1.15zM8.13 17.79v2.52h1.56v-2.52L11.82 19l.77-1.2-2.16-1.33 2.12-1.28-.73-1.24-2.13 1.23v-2.56H8.13v2.56L6.05 14l-.78 1.2 2.16 1.3-2.16 1.33.78 1.17z' fill='currentColor' /></svg>
-                  console
-                </h1>
+          <nav className='flex-none w-64 bg-gray-900 text-white px-4 pb-4 border-r border-gray-800'>
+            <div className='flex flex-col justify-between min-h-full'>
+              <div class="flex-none">
+                <SpaceSelector />
               </div>
-              <SpaceSelector className='flex-none grow' />
+              <div>
+                <SpaceCreator className='mb-4' />
+                <Logo />
+              </div>
             </div>
           </nav>
-          <main className='grow bg-gray-100 dark:bg-dark-gray p-4'>
+          <main className='grow bg-dark-gray text-white p-4'>
             <SpaceSection />
           </main>
         </div>
