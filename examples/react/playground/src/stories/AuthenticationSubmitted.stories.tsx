f98c37ab773176a55c3b00fb8ae613535917c9b6
Travis Vachon <travis.vachon@gmail.com>
2023-01-26 14:32:49 -0800

    feat: add Authenticator to StoryBook
    port CSS from Tailwind to raw CSS, rename some classes, factor common CSS out to base.css

---
+++ examples/react/playground/src/stories/AuthenticationSubmitted.stories.tsx
@@ -0,0 +1,20 @@
+import { AuthenticationSubmitted } from '@w3ui/react'
+import '@w3ui/react/src/styles/authenticator.css'
+
+export default {
+  title: 'w3ui/AuthenticationSubmitted',
+  component: AuthenticationSubmitted,
+  tags: ['autodocs'],
+  argTypes: {
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+}
+
+export const Primary = {
+  args: {
+  }
+}
---
+++ examples/react/playground/src/stories/Authenticator.stories.tsx
@@ -0,0 +1,20 @@
+import { Authenticator } from '@w3ui/react'
+import '@w3ui/react/src/styles/authenticator.css'
+
+export default {
+  title: 'w3ui/Authenticator',
+  component: Authenticator,
+  tags: ['autodocs'],
+  argTypes: {
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+}
+
+export const Primary = {
+  args: {
+  }
+}
