27ff5d653f5c3115c85a15ddee720e6fb4c91bd4
Travis Vachon <travis.vachon@gmail.com>
2023-01-26 15:56:23 -0800

    feat: extract SpaceCreator to its own component
    get it into Storybook, extract css classes

---
+++ examples/react/playground/src/stories/SpaceCreator.stories.tsx
@@ -0,0 +1,20 @@
+import { SpaceCreator } from '@w3ui/react'
+import '@w3ui/react/src/styles/space-creator.css'
+
+export default {
+  title: 'w3ui/SpaceCreator',
+  component: SpaceCreator,
+  tags: ['autodocs'],
+  argTypes: {
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+}
+
+export const Primary = {
+  args: {
+  }
+}
---
+++ examples/react/playground/src/stories/SpaceCreatorCreating.stories.tsx
@@ -0,0 +1,20 @@
+import { SpaceCreatorCreating } from '@w3ui/react'
+import '@w3ui/react/src/styles/space-creator.css'
+
+export default {
+  title: 'w3ui/SpaceCreatorCreating',
+  component: SpaceCreatorCreating,
+  tags: ['autodocs'],
+  argTypes: {
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+}
+
+export const Primary = {
+  args: {
+  }
+}
