c29164da3486d0fdd597990cff080af9572e93e5
Travis Vachon <travis.vachon@gmail.com>
2023-02-03 10:48:25 +0800

    fix: clean up tests significantly
    @olizilla pointed me at https://kentcdodds.com/blog/common-mistakes-with-react-testing-library which helped me make this MUCH cleaner, woo hoo!

--- packages/react-keyring/test/Authenticator.spec.tsx
+++ packages/react-keyring/test/Authenticator.spec.tsx
@@ -1,55 +1,33 @@
 import React from 'react'
-import { test, expect, vi, beforeEach, afterEach } from 'vitest'
 import 'fake-indexeddb/auto'
-import userEvent from '@testing-library/user-event'
-import { render, screen } from '@testing-library/react' // (or /dom, /vue, ...)
-import { unmountComponentAtNode } from 'react-dom'
-import { act } from 'react-dom/test-utils'
+import { test, expect, vi } from 'vitest'
+import user from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react'
 
 import { KeyringContext, keyringContextDefaultValue, KeyringContextValue } from '../src/providers/Keyring'
 import { Authenticator, CancelButton, Form, EmailInput } from '../src/index'
 
-let container: HTMLDivElement = document.createElement('div')
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement('div')
-  document.body.appendChild(container)
-})
-
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container)
-  container.remove()
-})
-
 test('CancelButton', async () => {
-  const user = userEvent.setup()
-
   const cancelRegisterSpace = vi.fn()
   const contextValue: KeyringContextValue = [
     keyringContextDefaultValue[0],
     { ...keyringContextDefaultValue[1], cancelRegisterSpace }
   ]
-  act(() => {
-    render((
-      <KeyringContext.Provider value={contextValue}>
-        <Authenticator>
-          <CancelButton>Cancel</CancelButton>
-        </Authenticator>
-      </KeyringContext.Provider>
-    ), container)
-  })
+  render(
+    <KeyringContext.Provider value={contextValue}>
+      <Authenticator>
+        <CancelButton>Cancel</CancelButton>
+      </Authenticator>
+    </KeyringContext.Provider>
+  )
 
-  const cancelButton = screen.getByRole('button')
-  expect(cancelButton.textContent).toBe('Cancel')
+  const cancelButton = screen.getByText('Cancel')
   await user.click(cancelButton)
 
   expect(cancelRegisterSpace).toHaveBeenCalledOnce()
 })
 
 test('Form', async () => {
-  const user = userEvent.setup()
-
   const createSpace = vi.fn()
   const registerSpace = vi.fn()
 
@@ -61,26 +39,23 @@ test('Form', async () => {
       registerSpace
     }
   ]
-  act(() => {
-    render((
-      <KeyringContext.Provider value={contextValue}>
-        <Authenticator>
-          <Form>
-            <EmailInput placeholder='Email' />
-            <button type='submit' value='Create Space' data-testid='submit' />
-          </Form>
-        </Authenticator>
-      </KeyringContext.Provider>
-    ), container)
-  })
+  render(
+    <KeyringContext.Provider value={contextValue}>
+      <Authenticator>
+        <Form>
+          <EmailInput placeholder='Email' />
+          <input type='submit' value='Create Space' />
+        </Form>
+      </Authenticator>
+    </KeyringContext.Provider>
+  )
 
   const myEmail = 'travis@dag.house'
   const emailInput = screen.getByPlaceholderText('Email')
   await user.click(emailInput)
   await user.keyboard(myEmail)
 
-  const submitButton = screen.getByTestId('submit')
-  expect(submitButton.value).toBe('Create Space')
+  const submitButton = screen.getByText('Create Space')
   await user.click(submitButton)
 
   expect(createSpace).toHaveBeenCalledOnce()
