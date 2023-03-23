04990c57e3575d0c12ff1eb7420d0d64c5415c26
Travis Vachon <travis@dag.house>
2023-03-23 10:41:35 -0700

    fix: use production service configuration by default in w3console (#425)

--- examples/react/w3console/src/components/services.ts
+++ examples/react/w3console/src/components/services.ts
@@ -6,11 +6,11 @@ import * as DID from '@ipld/dag-ucan/did'
 
 export const accessServiceURL = new URL(
   //'https://w3access-staging.protocol-labs.workers.dev'
-  import.meta.env.VITE_W3UP_ACCESS_SERVICE_URL
+  import.meta.env.VITE_W3UP_ACCESS_SERVICE_URL ?? 'https://access.web3.storage'
 )
 export const accessServicePrincipal = DID.parse(
   //'did:web:staging.web3.storage'
-  import.meta.env.VITE_W3UP_ACCESS_SERVICE_DID
+  import.meta.env.VITE_W3UP_ACCESS_SERVICE_DID ?? 'did:web:web3.storage'
 )
 
 export const accessServiceConnection = connect<Service>({
@@ -25,11 +25,11 @@ export const accessServiceConnection = connect<Service>({
 
 export const uploadServiceURL = new URL(
   //'https://staging.up.web3.storage'
-  import.meta.env.VITE_W3UP_UPLOAD_SERVICE_URL
+  import.meta.env.VITE_W3UP_UPLOAD_SERVICE_URL ?? 'https://up.web3.storage'
 )
 export const uploadServicePrincipal = DID.parse(
   //'did:web:staging.web3.storage'
-  import.meta.env.VITE_W3UP_UPLOAD_SERVICE_DID
+  import.meta.env.VITE_W3UP_UPLOAD_SERVICE_DID ?? 'did:web:web3.storage'
 )
 
 export const uploadServiceConnection = connect<Service>({
