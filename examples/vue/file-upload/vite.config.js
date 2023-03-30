0925c213c68c17f7edf606c0e55d22cec85b719a
Travis Vachon <travis@dag.house>
2023-03-30 08:13:16 -0700

    fix!: update examples for new authorize flow (#474)
    Unfortunately this also required some changes to the "keyring"
    interfaces - shipping another major release based on these changes is
    slightly annoying but probably the right thing to do, and honestly I'm
    probably the only person who will need to upgrade deps for now.

--- examples/vue/file-upload/vite.config.js
+++ examples/vue/file-upload/vite.config.js
@@ -7,6 +7,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '',
   plugins: [vue()],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
