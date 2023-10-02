29beb3708634123ce63619aa78de35835752f2c4
Travis Vachon <travis.vachon@protocol.ai>
2023-10-02 12:37:15 -0700

    feat: use two env vars to configure service not four (#562)
    unify service configs for w3console

--- examples/react/w3console/src/components/services.ts
+++ examples/react/w3console/src/components/services.ts
@@ -1,41 +1,23 @@
 import type { Service } from '@web3-storage/access/types'
 import { connect } from '@ucanto/client'
-import { CAR, CBOR, HTTP } from '@ucanto/transport'
+import { CAR, HTTP } from '@ucanto/transport'
 import * as DID from '@ipld/dag-ucan/did'
 
 
-export const accessServiceURL = new URL(
+export const serviceURL = new URL(
   //'https://staging.up.web3.storage'
-  import.meta.env.VITE_W3UP_ACCESS_SERVICE_URL ?? 'https://access.web3.storage'
+  import.meta.env.VITE_W3UP_SERVICE_URL ?? 'https://up.web3.storage'
 )
-export const accessServicePrincipal = DID.parse(
+export const servicePrincipal = DID.parse(
   //'did:web:staging.web3.storage'
-  import.meta.env.VITE_W3UP_ACCESS_SERVICE_DID ?? 'did:web:web3.storage'
+  import.meta.env.VITE_W3UP_SERVICE_DID ?? 'did:web:web3.storage'
 )
 
-export const accessServiceConnection = connect<Service>({
-  id: accessServicePrincipal,
+export const serviceConnection = connect<Service>({
+  id: servicePrincipal,
   codec: CAR.outbound,
   channel: HTTP.open<Record<string, any>>({
-    url: accessServiceURL,
-    method: 'POST',
-  }),
-})
-
-export const uploadServiceURL = new URL(
-  //'https://staging.up.web3.storage'
-  import.meta.env.VITE_W3UP_UPLOAD_SERVICE_URL ?? 'https://up.web3.storage'
-)
-export const uploadServicePrincipal = DID.parse(
-  //'did:web:staging.web3.storage'
-  import.meta.env.VITE_W3UP_UPLOAD_SERVICE_DID ?? 'did:web:web3.storage'
-)
-
-export const uploadServiceConnection = connect<Service>({
-  id: uploadServicePrincipal,
-  codec: CAR.outbound,
-  channel: HTTP.open<Record<string, any>>({
-    url: uploadServiceURL,
+    url: serviceURL,
     method: 'POST',
   }),
 })
