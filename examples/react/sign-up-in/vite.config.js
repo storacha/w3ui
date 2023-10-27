96b54e0e107f811d4449757e378d2617008c082d
Travis Vachon <travis.vachon@gmail.com>
2023-10-27 13:40:43 -0700

    fix: tell all the examples to be chill with top level await

--- examples/react/sign-up-in/vite.config.js
+++ examples/react/sign-up-in/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    supported: {
+      'top-level-await': true,
+    },
+  },
   base: '',
   plugins: [react()],
   server: {
