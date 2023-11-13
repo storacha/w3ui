5993aa1b5abff6b3c74a09e6275e23d133faa48d
Travis Vachon <travis.vachon@gmail.com>
2023-11-13 18:14:49 +0300

    fix: stray console.logs

--- packages/keyring-core/test/client.spec.ts
+++ packages/keyring-core/test/client.spec.ts
@@ -4,7 +4,6 @@ import 'fake-indexeddb/auto'
 import { createClient } from '../src/index.js'
 
 test('createClient', async () => {
-  console.log("CREATE CLIENT")
   const client = await createClient()
   expect(client).toBeTruthy()
   expect(client.did().startsWith('did:key')).toBe(true)
@@ -12,8 +11,6 @@ test('createClient', async () => {
 })
 
 test('createSpace', async () => {
-  console.log("CREATE SPACE")
-
   const client = await createClient()
   const space = await client.createSpace('test')
   expect(space).toBeTruthy()
