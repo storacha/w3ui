2ec46dc141226894963d0330f0b0860664e84e1f
Travis Vachon <travis.vachon@gmail.com>
2023-12-19 14:13:47 -0800

    feat: add uploads list example

--- examples/test/playwright/src/uploads-list.spec.ts
+++ examples/test/playwright/src/uploads-list.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test'
 
 for (const ui of [
-  //  'react'
+  'react'
 ]) {
   test(`${ui}: uploads list`, async ({ page }) => {
     await page.goto(`/${ui}/uploads-list/`)
     await expect(page).toHaveTitle('W3UI Uploads List Example App')
 
-    const input = page.getByRole('textbox', { name: 'Email address:' })
+    const input = page.getByRole('textbox', { name: 'Email' })
     await input.fill('test@example.org')
     await input.press('Enter')
     await expect(page.getByText('Verify your email address!')).toBeVisible()
